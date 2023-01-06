d7063741fd5967a467f17ff4e58e569cca6caef4
Maria Shukshina <shukshina.mariia@gmail.com>
2023-01-06 20:05:00 +0100

    Hard coded companies + setting up fragments

--- app/src/main/java/com/example/discountcardsapplication/MainActivity.kt
+++ app/src/main/java/com/example/discountcardsapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.discountcardsapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.discountcardsapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navController = Navigation.findNavController(this, R.id.host_fragment)
+        NavigationUI.setupWithNavController(binding.bottomNav, navController)
     }
 }

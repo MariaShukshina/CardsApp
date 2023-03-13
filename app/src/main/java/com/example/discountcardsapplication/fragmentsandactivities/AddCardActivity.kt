2b433692127b5e1112bcb08396933ae283d730d0
Maria Shukshina <shukshina.mariia@gmail.com>
2023-03-13 19:42:48 +0100

    Modifying night theme

--- app/src/main/java/com/example/discountcardsapplication/fragmentsandactivities/AddCardActivity.kt
+++ app/src/main/java/com/example/discountcardsapplication/fragmentsandactivities/AddCardActivity.kt
@@ -28,7 +28,7 @@ import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.ViewModelProvider
 import com.example.discountcardsapplication.R
 import com.example.discountcardsapplication.database.CardsDatabase
-import com.example.discountcardsapplication.databinding.ActivityAddOrEditCardBinding
+import com.example.discountcardsapplication.databinding.ActivityAddCardBinding
 import com.example.discountcardsapplication.fragmentsandactivities.ChooseCompanyActivity.Companion.COMPANY_IMAGE
 import com.example.discountcardsapplication.fragmentsandactivities.ChooseCompanyActivity.Companion.COMPANY_NAME
 import com.example.discountcardsapplication.fragmentsandactivities.ScanCardActivity.Companion.BARCODE_FORMAT
@@ -51,7 +51,7 @@ import java.io.OutputStream
 import java.util.*
 
 class AddCardActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityAddOrEditCardBinding
+    private lateinit var binding: ActivityAddCardBinding
     private var companyName: String = ""
     private var imageResource: Int = 0
     private var customImage: Uri? = null
@@ -87,7 +87,7 @@ class AddCardActivity : AppCompatActivity() {
             false
         )
 
-        binding = ActivityAddOrEditCardBinding.inflate(layoutInflater)
+        binding = ActivityAddCardBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.etCompanyName.addTextChangedListener {
             setupButtonDoneColor()
